b460568be69ada79fcbc13bb86bca97411b9ad39
Thanh Son Truong <ttru0037@student.monash.edu>
2025-04-01 15:59:20 +1100

    feat: implement number guessing game
    Added a console-based number guessing game in Java where the player tries to guess a random number between 1 and 100. Includes feedback on guesses and tracks the number of attempts.

---
+++ src/src/NumberGuessingGame.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+        int numberToGuess = rand.nextInt(100) + 1; // Random number between 1 and 100
+        int guess = 0;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100:");
+
+        while (guess != numberToGuess) {
+            System.out.print("Your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Correct! You guessed the number in " + attempts + " attempts.");
+            }
+        }
+
+        scanner.close();
+    }
+}
