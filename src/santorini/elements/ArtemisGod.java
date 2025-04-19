e6fd4fc17344146b9cbd3c0ad193bc3bbdd947c3
nlee0075 <nlee0075@student.monash.edu>
2025-04-19 21:25:40 +1000

    ArtemisGod card class

---
+++ src/santorini/elements/ArtemisGod.java
@@ -0,0 +1,52 @@
+package santorini.elements;
+
+import santorini.board.Cell;
+import santorini.engine.Player;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArtemisGod extends GodCard {
+    private Cell firstMoveCell;
+    private boolean isSecondMove = false;
+
+    public ArtemisGod(Player player) {
+        super("Artemis", "Your Worker may move one additional time, but not back to the initial space.", player);
+    }
+    @Override
+    public void onTurnStart() {
+        // Setup for Artemis at the beginning of the turn
+        super.onTurnStart();
+        firstMoveCell = null;
+        isSecondMove = false;
+    }
+
+    public void setFirstMoveCell(Cell cell)
+    {
+        this.firstMoveCell = cell;
+        this.isSecondMove = true;
+    }
+
+    @Override
+    public List<Cell> modifyMoveOptions(Worker worker, List<Cell> defaultOptions) {
+        if (isSecondMove && firstMoveCell != null) {
+            List<Cell> modified = new ArrayList<>(defaultOptions);
+            modified.removeIf(cell -> cell.equals(firstMoveCell)); // Prevent moving back to the initial space
+            return modified;
+        }
+        return defaultOptions; // If it's the first move, use default options}    }
+    }
+    @Override
+    public List<Cell> modifyBuildOptions(Worker worker, List<Cell> defaultOptions) {
+        return defaultOptions; // Artemis does not affect building options
+    }
+
+    @Override
+    public void onTurnEnd() {
+        // Clean up after the turn ends
+        super.onTurnEnd();
+        firstMoveCell = null;
+        isSecondMove = false;
+    }
+
+}
