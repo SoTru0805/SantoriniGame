8eb848f5a4417ff807a8f25612b1babe30ca7f3f
Nguyen Viet Phuc <vngu0096@student.monash.edu>
2025-04-20 21:09:55 +1000

    Modified GodCard and Player classes: - Add a sample GodCard and Player codes to be called in Main - They are on developing state and being compared with other member's progress.

--- src/santorini/engine/Player.java
+++ src/santorini/engine/Player.java
@@ -1,4 +1,39 @@
-package santorini.engine;
-
-public class Player {
-}
+//package santorini.engine;
+//
+//import santorini.elements.Worker;
+//import santorini.elements.GodCard;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Player {
+//  private String name;
+//  private GodCard godCard;
+//  private List<Worker> workers;
+//
+//  public Player(String name) {
+//    this.name = name;
+//    this.workers = new ArrayList<>();
+//  }
+//
+//  public String getName() {
+//    return name;
+//  }
+//
+//  public void setGodCard(GodCard godCard) {
+//    this.godCard = godCard;
+//    godCard.setPlayer(this); // Link god card back to this player
+//  }
+//
+//  public GodCard getGodCard() {
+//    return godCard;
+//  }
+//
+//  public void addWorker(Worker worker) {
+//    workers.add(worker);
+//  }
+//
+//  public List<Worker> getWorkers() {
+//    return workers;
+//  }
+//}
