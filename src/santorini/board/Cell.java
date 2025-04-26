12a725b969dc337b640da76135b0debcde66f369
Chia Yaw Chong <ccho0090@student.monash.edu>
2025-04-26 20:45:25 +1000

    Refactor: Add Cell and Building classes
    This commit introduces the `Cell` and `Building` classes to
    represent elements on the Santorini game board. Additionally, it
    includes minor changes such as adding the macOS .DS_Store file to
    .gitignore.

--- src/santorini/board/Cell.java
+++ src/santorini/board/Cell.java
@@ -1,5 +1,8 @@
 package santorini.board;
+import santorini.elements.Worker;
+import santorini.elements.Building;
 
+<<<<<<< Updated upstream
 
 public class Cell {
     private final int x;
@@ -14,14 +17,54 @@ public class Cell {
         this.hasWorker = false;
     }
 
+=======
+/**
+ * Represents a cell on the Santorini game board.
+ * Each cell can have a building and/or a worker.
+ */
+public class Cell {
+
+    private int x; // Coordinate on the board
+    private int y; // Coordinate on the board
+    private Building buildingOnCell; // Building on this cell (can be null)
+    private Worker worker; // Worker on this cell (can be null)
+
+    /**
+     * Constructs a new Cell with coordinates.
+     *
+     * @param x The x-coordinate of the cell.
+     * @param y The y-coordinate of the cell.
+     */
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.buildingOnCell = null; // No building initially
+        this.worker = null; // No worker initially
+    }
+
+    /**
+     * Gets the x-coordinate of the cell.
+     *
+     * @return The x-coordinate.
+     */
+>>>>>>> Stashed changes
     public int getX() {
         return x;
     }
 
+<<<<<<< Updated upstream
+=======
+    /**
+     * Gets the y-coordinate of the cell.
+     *
+     * @return The y-coordinate.
+     */
+>>>>>>> Stashed changes
     public int getY() {
         return y;
     }
 
+<<<<<<< Updated upstream
     public boolean isOccupied() {
         return hasWorker;
     }
@@ -51,3 +94,91 @@ public class Cell {
         this.hasDome = hasDome;
     }
 }
+=======
+    /**
+     * Gets the building on this cell.
+     *
+     * @return The Building object, or null if there is no building.
+     */
+    public Building getBuildingOnCell() {
+        return buildingOnCell;
+    }
+
+    /**
+     * Sets the building on this cell.
+     *
+     * @param buildingOnCell The Building object to set.
+     */
+    public void setBuildingOnCell(Building buildingOnCell) {
+        this.buildingOnCell = buildingOnCell;
+    }
+
+    /**
+     * Gets the worker on this cell.
+     *
+     * @return The Worker object, or null if the cell is not occupied.
+     */
+    public Worker getWorker() {
+        return worker;
+    }
+
+    /**
+     * Sets the worker on this cell.
+     *
+     * @param worker The Worker object to set.
+     */
+    public void setWorker(Worker worker) {
+        this.worker = worker;
+    }
+
+    /**
+     * Removes the worker from this cell.
+     */
+    public void clearWorker() {
+        this.worker = null;
+    }
+
+    /**
+     * Checks if the cell is occupied by a worker.
+     *
+     * @return True if a worker is on the cell, false otherwise.
+     */
+    public boolean isOccupied() {
+        return worker != null;
+    }
+
+    /**
+     * Checks if a worker can move onto this cell.
+     *
+     * @param worker The worker trying to move.
+     * @return True if the worker can move onto the cell, false otherwise.
+     */
+    public boolean canMoveTo(Worker worker) {
+        return this.worker == null && (buildingOnCell == null || !buildingOnCell.hasDome());
+    }
+
+    /**
+     * Checks if a building can be added or leveled up on this cell.
+     *
+     * @return True if building is allowed, false otherwise.
+     */
+    public boolean canBuild() {
+        return buildingOnCell != null ? buildingOnCell.getLevel() < 3 && !buildingOnCell.hasDome() : true;
+    }
+
+    /**
+     * Returns a string representation of the cell.
+     *
+     * @return A string containing cell information.
+     */
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "x=" + x +
+                ", y=" + y +
+                ", buildingOnCell=" + buildingOnCell +
+                ", worker=" + (worker != null ? worker.getId() : "null") +
+                '}';
+    }
+}
+>>>>>>> Stashed changes
--- src/santorini/elements/Worker.java
+++ src/santorini/elements/Worker.java
@@ -1,5 +1,112 @@
 package santorini.elements;
 
+import santorini.engine.Player;
+import santorini.board.Cell;
+
+/**
+ * Represents a worker in the Santorini game.
+ * Each worker belongs to a player and has a current location.
+ */
 public class Worker {
 
+<<<<<<< Updated upstream
 }
+=======
+    private int id;
+    private Cell currentLocation;
+    private Player player;
+    private Cell previousLocation;
+
+    /**
+     * Constructs a new Worker.
+     * @param id The unique identifier for this worker.
+     * @param player The player who owns this worker.
+     */
+    public Worker(int id, Player player) {
+        this.id = id;
+        this.player = player;
+        this.currentLocation = null;
+        this.previousLocation = null;
+    }
+
+    /**
+     * Gets the unique identifier of this worker.
+     * @return The worker's ID.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Gets the current location of this worker.
+     * @return The Cell where the worker is currently located.
+     */
+    public Cell getCurrentLocation() {
+        return currentLocation;
+    }
+
+    /**
+     * Sets the current location of this worker.
+     * @param currentLocation The Cell to set as the worker's current location.
+     */
+    public void setCurrentLocation(Cell currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    /**
+     * Gets the player who owns this worker.
+     * @return The Player object.
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * Gets the previous location of this worker.
+     * @return The Cell where the worker was previously located.
+     */
+    public Cell getPreviousLocation() {
+        return previousLocation;
+    }
+
+    /**
+     * Sets the previous location of this worker.
+     * @param previousLocation The Cell to set as the worker's previous location.
+     */
+    public void setPreviousLocation(Cell previousLocation) {
+        this.previousLocation = previousLocation;
+    }
+
+    /**
+     * Moves the worker to a new cell.
+     * NOTE:  Validation should be done by the Player class!
+     * @param newLocation The Cell to move the worker to.
+     */
+    public void moveTo(Cell newLocation) {
+        this.previousLocation = this.currentLocation;
+        this.currentLocation = newLocation;
+    }
+
+    /**
+     * Builds a block on a cell.
+     * NOTE: Validation should be done by the Player class!
+     * @param cell The Cell to build on.
+     */
+    public void buildBlock(Cell cell) {
+        //  TODO: Implement the logic to build a block on the cell
+        //  This might involve updating the cell's level or structure.
+        System.out.println("Worker " + id + " built on cell: " + cell);
+    }
+
+    /**
+     * Undoes the last move.
+     * NOTE: This assumes we only need to undo one move. More complex undo might need a stack.
+     */
+    public void undoMove() {
+        if (previousLocation != null) {
+            currentLocation = previousLocation;
+            previousLocation = null; //  Clear the previous location after undoing
+        }
+    }
+}
+>>>>>>> Stashed changes
